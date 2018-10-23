35850aeea0dc98c05e98996af79c4b8a027997af
Sam Jewell <sam@bridge-u.com>
2018-10-23 09:25:48 +0100

    Solve all of the exercises

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ActiveRecord::Base
   belongs_to :region
   has_many :people
+
+  scope :order_by_name, -> { order(:name) }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -5,14 +5,19 @@ class Person < ActiveRecord::Base
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
   def self.order_by_location_name
-    all
+    # joins(:location).order("locations.name")
+    # joins(:location).order(Location.arel_table[:name])
+    joins(:location).merge(Location.order_by_name)
   end
 
   def self.with_employees
-    all
+    # raise joins(:employees).distinct.to_sql
+    joins(:employees).distinct
   end
 
   def self.with_employees_order_by_location_name
-    all
+    # with_employees.order_by_location_name
+    # from(self.with_employees, :people).order_by_location_name
+    from(with_employees, arel_table.name).order_by_location_name
   end
 end
